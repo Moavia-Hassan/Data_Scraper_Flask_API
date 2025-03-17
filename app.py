e6226bb842f100888f8bc4f9e25915e4cde1e5d5
Moavia Hassan <moaviahassan112@gmail.com>
2025-03-17 12:26:07 +0500

    updated app.py for deployment on vercel

--- app.py
+++ app.py
@@ -69,4 +69,5 @@ def search():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    # app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
